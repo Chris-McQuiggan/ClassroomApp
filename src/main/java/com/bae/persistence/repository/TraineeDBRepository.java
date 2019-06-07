41a1f17f7f9c0548690db6dda0073161d931f503
jm4clark <jamesmclark96@gmail.com>
2019-06-07 12:54:53 +0100

    created getATrainee for trainee Database repo

--- src/main/java/com/bae/persistence/repository/TraineeDBRepository.java
+++ src/main/java/com/bae/persistence/repository/TraineeDBRepository.java
@@ -1,6 +1,18 @@
 package com.bae.persistence.repository;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.bae.persistence.domain.Trainee;
+import com.bae.util.JSONUtil;
+
 public class TraineeDBRepository implements TraineeRepository {
+
+	@Inject
+	JSONUtil util;
+	@PersistenceContext(unitName = "primary")
+	private EntityManager em;
 
 	@Override
 	public String addTrainee(String trainee) {
@@ -16,8 +28,7 @@ public class TraineeDBRepository implements TraineeRepository {
 
 	@Override
 	public String getATrainee(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return util.getJSONForObject(em.find(Trainee.class, id));
 	}
 
 	@Override
