f67198d142c6b669d3fc2950b8c4d0cec22fc6a5
jm4clark <jamesmclark96@gmail.com>
2019-06-07 15:25:50 +0100

    altered Classroom to support join

--- src/main/java/com/bae/persistence/domain/Classroom.java
+++ src/main/java/com/bae/persistence/domain/Classroom.java
@@ -1,17 +1,23 @@
 package com.bae.persistence.domain;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Classroom {
 
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
-	private int classID;
+	int classID;
 	private String trainer;
+
+	@OneToMany(targetEntity = Trainee.class)
+	private List<Trainee> traineelist;
 
 	public Classroom() {
 
@@ -33,5 +39,13 @@ public class Classroom {
 
 	public void setTrainer(String trainer) {
 		this.trainer = trainer;
+	}
+
+	public List<Trainee> getTraineelist() {
+		return traineelist;
+	}
+
+	public void setTraineelist(List<Trainee> traineeListIn) {
+		this.traineelist = traineeListIn;
 	}
 }
